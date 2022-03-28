ba0625381db6e826687fdb009aa7aa21ebc96e06
agustingeger16 <agustingeger@gmail.com>
2022-03-28 22:25:50 +0700

    menambahakan tugas pertemuan 1 contoh1

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> perkenalkan </h1>";
+		echo "nama saya geger giat agustina, saya tinggal di daerah kebon jeruk - jakarta barat, olah raga yang saya sukai adalah maen catur"
+	}
+}
